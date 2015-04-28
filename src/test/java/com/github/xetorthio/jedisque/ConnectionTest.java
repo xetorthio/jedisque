b3ea5e4402efb4a4ec434a38fecb416afef561f3
Marcos Lilljedahl <marcosnils@gmail.com>
2015-04-28 16:58:34 -0300

    Change test strategy in favor of simple testing Update project source to 1.6

--- src/test/java/com/github/xetorthio/jedisque/ConnectionTest.java
+++ src/test/java/com/github/xetorthio/jedisque/ConnectionTest.java
@@ -8,20 +8,19 @@ import org.junit.Test;
 import redis.clients.jedis.exceptions.JedisConnectionException;
 
 public class ConnectionTest {
+
 	@Test
 	public void iterateOverHosts() throws URISyntaxException {
-		try (Jedisque q = new Jedisque(new URI("disque://localhost:55665"),
-				new URI("disque://localhost:7711"))) {
-			q.info();
-		}
+		Jedisque q = new Jedisque(new URI("disque://localhost:55665"), new URI("disque://localhost:7711"));
+		q.info();
+		q.close();
+
 	}
 
 	@Test(expected = JedisConnectionException.class)
-	public void throwExceptionWhenNodesAreUnavailbale()
-			throws URISyntaxException {
-		try (Jedisque q = new Jedisque(new URI("disque://localhost:55665"),
-				new URI("disque://localhost:55666"))) {
-			q.info();
-		}
+	public void throwExceptionWhenNodesAreUnavailbale() throws URISyntaxException {
+		Jedisque q = new Jedisque(new URI("disque://localhost:55665"), new URI("disque://localhost:55666"));
+		q.info();
+		q.close();
 	}
 }
