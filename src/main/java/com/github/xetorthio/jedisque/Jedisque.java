9ab33833e85f1bc14309174bd3545a0ae1b0e0ff
Marcos Lilljedahl <marcosnils@gmail.com>
2015-04-29 11:34:21 -0300

    Add paramters to addjob command

--- src/main/java/com/github/xetorthio/jedisque/Jedisque.java
+++ src/main/java/com/github/xetorthio/jedisque/Jedisque.java
@@ -1,9 +1,14 @@
 package com.github.xetorthio.jedisque;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
@@ -49,6 +54,16 @@ public class Jedisque extends redis.clients.jedis.Connection {
 
 	public String addJob(String queueName, String job, int mstimeout) {
 		sendCommand(Commands.ADDJOB, SafeEncoder.encodeMany(queueName, job, String.valueOf(mstimeout)));
+		return getBulkReply();
+	}
+
+	public String addJob(String queueName, String job, int mstimeout, JobParams params) {
+		List<String> addJobCommand = new LinkedList<String>();
+		addJobCommand.add(queueName);
+		addJobCommand.add(job);
+		addJobCommand.add(String.valueOf(mstimeout));
+		addJobCommand.addAll(getJobParamsAsList(params));
+		sendCommand(Commands.ADDJOB, addJobCommand.toArray(new String[addJobCommand.size()]));
 		return getBulkReply();
 	}
 
@@ -106,9 +121,36 @@ public class Jedisque extends redis.clients.jedis.Connection {
 		return getIntegerReply();
 	}
 	
+	
+	private List<String> getJobParamsAsList(JobParams params) {
+		List<String> jobParams = new LinkedList<String>();
+		if (params.getReplicate() != null) {
+			jobParams.add(SafeEncoder.encode(Keyword.REPLICATE.raw));
+			jobParams.add(String.valueOf(params.getReplicate()));
+		} else if (params.getDelay() != null) {
+			jobParams.add(SafeEncoder.encode(Keyword.DELAY.raw));
+			jobParams.add(String.valueOf(params.getDelay()));
+		} else if (params.getRetry() != null) {
+			jobParams.add(SafeEncoder.encode(Keyword.RETRY.raw));
+			jobParams.add(String.valueOf(params.getRetry()));
+		} else if (params.getTTL() != null) {
+			jobParams.add(SafeEncoder.encode(Keyword.TTL.raw));
+			jobParams.add(String.valueOf(params.getTTL()));
+		} else if (params.getMaxlen() != null) {
+			jobParams.add(SafeEncoder.encode(Keyword.MAXLEN.raw));
+			jobParams.add(String.valueOf(params.getMaxlen()));
+		} else if (params.getAsync() != null && params.getAsync().booleanValue()) {
+			jobParams.add(String.valueOf(Keyword.ASYNC.raw));
+		}
+		
+		
+		return jobParams;
+	}
+
+
 
 	private enum Keyword {
-		FROM;
+		FROM, REPLICATE, DELAY, RETRY, TTL, MAXLEN, ASYNC;
 		private final byte[] raw;
 
 		Keyword() {
--- src/test/java/com/github/xetorthio/jedisque/JedisqueTest.java
+++ src/test/java/com/github/xetorthio/jedisque/JedisqueTest.java
@@ -33,6 +33,15 @@ public class JedisqueTest {
 		String jobId = q.addJob(getQueueName(), "message", 10);
 		assertNotNull(jobId);
 	}
+	
+
+	@Test
+	public void addJobWithParams() {
+		JobParams params = new JobParams(1, 10, 10, 10, 10, true);
+		String jobId = q.addJob(getQueueName(), "message", 10, params);
+		assertNotNull(jobId);
+	}
+
 
 	@Test
 	public void getJob() {
